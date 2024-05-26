54c2fb6ca92f923cfa229913450ba248db321e66
ElsayedAbdo219 <131375012+ElsayedAbdo219@users.noreply.github.com>
2024-05-26 13:41:23 +0200

    install sanctum

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+    protected $guarded=[];
+
+    public function Post(){
+        return $this->belongsTo(Company::class,'company_id');
+    }
 }
