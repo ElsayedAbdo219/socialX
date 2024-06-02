6c6aa879c73829c027ba787f919ea7e490856164
ElsayedAbdo219 <131375012+ElsayedAbdo219@users.noreply.github.com>
2024-06-02 11:55:59 +0200

    fix dashboard

--- resources/views/dashboard/news/edit.blade.php
+++ resources/views/dashboard/news/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('components.dashboard.layouts.master')
 @section('title')
-    {{__('dashboard.goods')}}
+    {{__('dashboard.news')}}
 @endsection
 <!-- BEGIN: Content-->
 @section('content')
@@ -8,9 +8,9 @@
         <div class="content-overlay"></div>
         <div class="header-navbar-shadow"></div>
         <div class="content-wrapper">
-            <x-dashboard.layouts.breadcrumb now="{{$goodType->name}}">
-                <li class="breadcrumb-item"><a href="{{route('admin.goods-types.index')}}">
-                        {{__('dashboard.goods_list')}}
+            <x-dashboard.layouts.breadcrumb now="{{$News->name}}">
+                <li class="breadcrumb-item"><a href="{{route('admin.news.index')}}">
+                        {{__('dashboard.news_list')}}
                     </a></li>
             </x-dashboard.layouts.breadcrumb>
             <div class="col-12 mt-3">
@@ -20,18 +20,28 @@
                     </div>
                     <div class="card-content">
                         <div class="card-body">
-                            <form class="form form-vertical" method="POST" action="{{route('admin.goods-types.update',$goodType->id)}}" >
+                            <form class="form form-vertical" method="POST" action="{{route('admin.news.update',$News->id)}}" >
                                 @csrf
                                 @method('PATCH')
                                <div class="row">
                                     <div class="form-group col-sm-3">
-                                        <label for="name_en">{{__('dashboard.name')}}
-                                            <input type="text"  class="form-control" name="name" placeholder="{{__('dashboard.name')}}" value="{{$goodType->name ?? old('name')}}" />
-                                            @error('name')
+                                        <label for="title">{{__('dashboard.title')}}
+                                            <input type="text"  class="form-control" name="title" placeholder="{{__('dashboard.title')}}" value="{{$News->title ?? old('title')}}" />
+                                            @error('title')
                                             <span style="font-size: 12px;" class="text-danger">{{$message}}</span>
                                             @enderror
                                         </label>
                                     </div>
+
+                                    <div class="form-group col-sm-3">
+                                        <label for="contentNews">{{__('dashboard.contentNews')}}
+                                            <input type="text"  class="form-control" name="contentNews" placeholder="{{__('dashboard.contentNews')}}" value="{{$News->contentNews ?? old('contentNews')}}" />
+                                            @error('contentNews')
+                                            <span style="font-size: 12px;" class="text-danger">{{$message}}</span>
+                                            @enderror
+                                        </label>
+                                    </div>
+
                            
                                 </div>
                                 <div class="col-12">
