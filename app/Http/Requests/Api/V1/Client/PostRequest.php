c11019119313d72ea0f58eac6d94fbf418f615a4
ElsayedAbdo219 <micro.elsayed219@gmail.com>
2025-05-17 15:54:10 +0200

    fix(advertise):add advertise

--- app/Http/Requests/Api/V1/Client/PostRequest.php
+++ app/Http/Requests/Api/V1/Client/PostRequest.php
@@ -21,24 +21,26 @@ class PostRequest extends FormRequest
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
     public function rules(): array
-{
-    $types = implode(',' ,array_keys(PostTypeEnum::toArray()));
-
-    $file = $this->file('file_name');
-    $isVideo = $file && $this->type == PostTypeEnum::ADVERTISE;
-
-    return [
-        'type' => ['required','in:'.$types],
-        'content' => ['nullable','string'],
-        'file_name' => ['nullable','mimes:jpeg,png,jpg,mp4,avi,mov'],
-        'period' => [Rule::requiredIf($isVideo), 'string'],
-        'resolution' => [Rule::requiredIf($isVideo), 'numeric', 'in:720,1080,1440'],
-        'start_time' => [Rule::requiredIf($isVideo), 'date_format:h:i'],
-        'end_time' => [Rule::requiredIf($isVideo), 'date_format:h:i'],
-        'start_date' => [Rule::requiredIf($isVideo), 'date'],
-        'end_date' => [Rule::requiredIf($isVideo), 'date'],
-        'coupon_code' => [Rule::requiredIf($isVideo), 'string'],
-    ];
-}
+    {
+        $types = implode(',', array_keys(PostTypeEnum::toArray()));
+    
+        $file = $this->file('file_name');
+        $videoExtensions = ['mp4', 'avi', 'mov'];
+        $isVideo = $file && in_array($file->getClientOriginalExtension(), $videoExtensions) && $this->type == PostTypeEnum::ADVERTISE;
+    
+        return [
+            'type' => ['required', 'in:'.$types],
+            'content' => ['nullable', 'string'],
+            'file_name' => ['nullable', 'mimes:jpeg,png,jpg,mp4,avi,mov'],
+            'period' => [Rule::requiredIf($isVideo), 'string'],
+            'resolution' => [Rule::requiredIf($isVideo), 'numeric', 'in:720,1080,1440'],
+            'start_time' => [Rule::requiredIf($isVideo), 'date_format:H:i'],
+            'end_time' => [Rule::requiredIf($isVideo), 'date_format:H:i'],
+            'start_date' => [Rule::requiredIf($isVideo), 'date'],
+            'end_date' => [Rule::requiredIf($isVideo), 'date'],
+            'coupon_code' => [Rule::requiredIf($isVideo), 'string'],
+        ];
+    }
+    
 
 }
