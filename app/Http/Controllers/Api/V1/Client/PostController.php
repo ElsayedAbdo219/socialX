f7938495dee4faced1431f5e403d219f1670710d
Elsayed Rabeaa <131375012+ElsayedAbdo219@users.noreply.github.com>
2025-06-10 03:12:44 +0300

    delete content of laravel log file

--- app/Http/Controllers/Api/V1/Client/PostController.php
+++ app/Http/Controllers/Api/V1/Client/PostController.php
@@ -323,14 +323,14 @@ public function addPostIntro(Request $request)
     $file = $request->file('file_name');
     $extension = strtolower($file->getClientOriginalExtension());
 
-    if (in_array($extension, ['mp4', 'avi', 'mov'])) {
-        $getID3 = new \getID3;
-        $analysis = $getID3->analyze($file->getRealPath());
+    // if (in_array($extension, ['mp4', 'avi', 'mov'])) {
+    //     $getID3 = new \getID3;
+    //     $analysis = $getID3->analyze($file->getRealPath());
 
-        if (isset($analysis['playtime_seconds']) && $analysis['playtime_seconds'] > 60) {
-            return response()->json(['message' => 'مدة الفيديو يجب أن لا تتجاوز 60 ثانية'], 422);
-        }
-    }
+    //     if (isset($analysis['playtime_seconds']) && $analysis['playtime_seconds'] > 60) {
+    //         return response()->json(['message' => 'مدة الفيديو يجب أن لا تتجاوز 60 ثانية'], 422);
+    //     }
+    // }
       // dd(auth('api')->user()->id);
     // ✅ حفظ الملف مؤقتًا
     $path = Storage::disk('public')->putFile('posts', $file);
