7574d379fbe4fe39421ae966a0f4ff2f1ff68cdc
Elsayed Rabeaa <131375012+ElsayedAbdo219@users.noreply.github.com>
2025-06-12 15:50:20 +0300

    fix accept(lang):refactor lang accept server

--- app/Http/Middleware/APILocale.php
+++ app/Http/Middleware/APILocale.php
@@ -16,11 +16,25 @@ class APILocale
    * @return mixed
    */
   public function handle(Request $request, Closure $next)
-{
+  {
     $supportedLocales = ['en', 'ar'];
-    $locale = $request->getPreferredLanguage($supportedLocales);
-    App::setLocale($locale ?? config('app.locale'));
+    $rawLocale = $request->getPreferredLanguage(['en_EG', 'en', 'ar_EG', 'ar']);
+
+    $map = [
+      'en_EG' => 'en',
+      'ar_EG' => 'ar',
+    ];
+
+    $locale = $map[$rawLocale] ?? $rawLocale ?? config('app.locale');
+
+    App::setLocale($locale);
+
     return $next($request);
+  }
 }
 
-}
+
+// $supportedLocales = ['en', 'ar'];
+//     $locale = $request->getPreferredLanguage($supportedLocales);
+//     App::setLocale($locale ?? config('app.locale'));
+//     return $next($request);
