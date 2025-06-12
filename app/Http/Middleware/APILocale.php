147289aafa723e62a4115f4237e57516e4d7c057
Elsayed Rabeaa <131375012+ElsayedAbdo219@users.noreply.github.com>
2025-06-12 15:18:32 +0300

    fix accept(lang):refactor lang accept server

--- app/Http/Middleware/APILocale.php
+++ app/Http/Middleware/APILocale.php
@@ -8,23 +8,19 @@ use Illuminate\Support\Facades\App;
 
 class APILocale
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
+  /**
+   * Handle an incoming request.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \Closure  $next
+   * @return mixed
+   */
   public function handle(Request $request, Closure $next)
 {
-    // حدد اللغات المدعومة في مشروعك
     $supportedLocales = ['en', 'ar'];
-
-    // Laravel هتختار أنسب لغة منهم بناءً على Accept-Language
     $locale = $request->getPreferredLanguage($supportedLocales);
-
     App::setLocale($locale ?? config('app.locale'));
-
     return $next($request);
 }
+
 }
