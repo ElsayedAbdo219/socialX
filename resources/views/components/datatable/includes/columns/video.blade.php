25647561ce11868660d2e7be9f225e412ea92dc1
Elsayed Rabeaa <131375012+ElsayedAbdo219@users.noreply.github.com>
2025-06-24 21:48:42 +0300

    fix(advertises):add border raduis to advertises vid(s)

--- resources/views/components/datatable/includes/columns/video.blade.php
+++ resources/views/components/datatable/includes/columns/video.blade.php
@@ -1,5 +1,5 @@
 @if ($video)
-  <video style="width: 150px;height: 150px" controls>
+  <video style="width: 150px;height: 150px; border-radius: 8px;" controls>
     <source src="{{ $video }}" type="{{ $type }}">
     المتصفح لا يدعم الفيديو.
   </video>
