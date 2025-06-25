b4632a5cb5946ea673a40ecbf28fb8d545876c06
Elsayed Rabeaa <131375012+ElsayedAbdo219@users.noreply.github.com>
2025-06-25 12:14:00 +0300

    feat(analysis):add analytics for sys

--- app/Http/Controllers/Api/V1/Client/JobController.php
+++ app/Http/Controllers/Api/V1/Client/JobController.php
@@ -195,7 +195,7 @@ class JobController extends Controller
     return [
        'companies' => Member::where('type', UserTypeEnum::COMPANY)->count(),
        'employees' => Member::where('type', UserTypeEnum::EMPLOYEE)->count(),
-       'users' => User::count(),
+       'users' => Member::count(),
        'jobs' => Job::OfStatus(1)->count(),
        'allEvents' => Calender::count(),
        'unCompletedEvents' => Calender::where('member_id', auth('api')->id())->where('status', '!=', 'completed')->count(),
