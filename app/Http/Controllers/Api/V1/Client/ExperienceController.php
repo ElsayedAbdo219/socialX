3f95efb358f0b40abb51a8b51e75e933fe6e57e0
Elsayed Rabeaa <131375012+ElsayedAbdo219@users.noreply.github.com>
2025-06-26 19:48:21 +0300

    fix(exp):fix validation off adding and updating

--- app/Http/Controllers/Api/V1/Client/ExperienceController.php
+++ app/Http/Controllers/Api/V1/Client/ExperienceController.php
@@ -12,139 +12,141 @@ use App\Notifications\ClientNotification;
 
 class ExperienceController extends Controller
 {
-    public function all(Request $request)
-    {
-        $paginateSize = $request->query('paginateSize', 10);
-        return response()->json(Experience::where('employee_id', auth('api')->user()->id)->with('company')->paginate($paginateSize));
+  public function all(Request $request)
+  {
+    $paginateSize = $request->query('paginateSize', 10);
+    return response()->json(Experience::where('employee_id', auth('api')->user()->id)->with('company')->paginate($paginateSize));
+  }
+
+  public function add(Request $request)
+  {
+    // return $request ; 
+    $data = $request->validate([
+      'title' => 'required|string|max:255',
+      'employment_type' => 'required|string|max:255',
+      'company_id' => 'nullable|integer|exists:members,id,type,' . UserTypeEnum::COMPANY . '|required_without:custom_company_name',
+      'custom_company_name' => 'nullable|string|max:255|required_without:company_id',
+      'city' => 'required|string|max:255',
+      'country' => 'required|string|max:255',
+      'location_type' => 'required|string|max:255',
+      'status_works' => 'required|string|max:255|in:work,notWork',
+      'still_working' => 'nullable|in:0,1',
+      'start_date' => 'required|numeric',
+      'end_date' => 'required_if:still_working,0|numeric',
+      'start_date_year' => 'required|numeric',
+      'end_date_year' => 'required_if:still_working,0|numeric|max:' . date("Y"),
+      'description' => 'required|string|max:255',
+      'profile_headline' => 'required|string|max:255',
+    ]);
+
+
+    $data['employee_id'] = auth('api')->user()->id;
+
+
+
+    $experience = Experience::create($data);
+    # sending a notification to the user  
+    if (!empty($data['company_id'])) {
+      $notifabels = Member::find($data['company_id']);
+      $notificationData = [
+        'title' => " اضافة تعليق عن موظف ",
+        'body' => "قام " . auth('api')->user()?->first_name . ' ' . auth('api')->user()?->last_name . " باضافتكم كشركة عمل بها!",
+        'type' => 'experience',
+        'id_link' => $experience?->employee_id,
+      ];
+
+      \Illuminate\Support\Facades\Notification::send(
+        $notifabels,
+        new ClientNotification($notificationData, ['database', 'firebase'])
+      );
     }
 
-    public function add(Request $request)
-    {
-        // return $request ; 
-        $data = $request->validate([
-            'title' => 'required|string|max:255',
-            'employment_type' => 'required|string|max:255',
-            'company_id' => 'required_if:custom_company_name,null|nullable|integer|exists:members,id,type,' . UserTypeEnum::COMPANY,
-            'custom_company_name' => 'required_if:company_id,null|string|max:255',
-            'city' => 'required|string|max:255',
-            'country' => 'required|string|max:255',
-            'location_type' => 'required|string|max:255',
-            'status_works' => 'required|string|max:255|in:work,notWork',
-            'still_working' => 'nullable|in:0,1',
-            'start_date' => 'required|numeric',
-            'end_date' => 'required_if:still_working,0|numeric',
-            'start_date_year' => 'required|numeric',
-            'end_date_year' => 'required_if:still_working,0|numeric|max:'.date("Y"),
-            'description' => 'required|string|max:255',
-            'profile_headline' => 'required|string|max:255',
-        ]);
+    return response()->json([
+      'message' => 'تم إضافة الخبرة بنجاح',
+      'experience' => $experience
+    ]);
+  }
 
-        $data['employee_id'] = auth('api')->user()->id;
+  public function get(Request $request, $member)
+  {
+    $paginateSize = $request->query('paginateSize', 10);
+    $member = Member::findOrFail($member);
+    return Experience::where('employee_id', $member->id)->with('company')->paginate($paginateSize);
+  }
 
-        
+  public function getAll($member)
+  {
+    $member = Member::findOrFail($member);
+    return Experience::where('employee_id', $member->id)->with('company')->get();
+  }
+  public function getCitiesWorked($Member)
+  {
+    $Member = Member::find($Member);
+    return array_unique($Member->experience()->pluck('city')->toArray());
+  }
 
-        $experience = Experience::create($data);
-         # sending a notification to the user  
-        if(!empty($data['company_id'])) {
-        $notifabels = Member::find($data['company_id']);
-        $notificationData = [
-            'title' => " اضافة تعليق عن موظف ",
-            'body' => "قام " . auth('api')->user()?->first_name.' '.auth('api')->user()?->last_name . " باضافتكم كشركة عمل بها!",
-            'type' => 'experience',
-            'id_link' => $experience?->employee_id, 
-        ];
+  public function show($experience)
+  {
+    $experience = Experience::findOrFail($experience);
+    return $experience;
+  }
 
-        \Illuminate\Support\Facades\Notification::send(
-            $notifabels,
-            new ClientNotification($notificationData, ['database', 'firebase'])
-        );
-      }
+  public function update(Request $request, $experience)
+  {
+    // return $request ;
+    $data = $request->validate([
+      'title' => 'required|string|max:255',
+      'employment_type' => 'required|string|max:255',
+      'company_id' => 'nullable|integer|exists:members,id,type,' . UserTypeEnum::COMPANY . '|required_without:custom_company_name',
+      'custom_company_name' => 'nullable|string|max:255|required_without:company_id',
 
-        return response()->json([
-            'message' => 'تم إضافة الخبرة بنجاح',
-            'experience' => $experience
-        ]);
+      'city' => 'required|string|max:255',
+      'country' => 'required|string|max:255',
+      'location_type' => 'required|string|max:255',
+      'status_works' => 'required|string|max:255|in:work,notWork',
+      'still_working' => 'nullable|in:0,1',
+      'start_date' => 'required|numeric',
+      'end_date' => 'required_if:still_working,0|numeric',
+      'start_date_year' => 'required|numeric',
+      'end_date_year' => 'required_if:still_working,0|numeric|max:' . date("Y"),
+      'description' => 'required|string|max:255',
+      'profile_headline' => 'required|string|max:255',
+    ]);
+    $experience = Experience::findOrFail($experience);
+    $data['employee_id'] = auth('api')->user()->id;
+
+
+
+    $experience->update($data);
+    # sending a notification to the user   
+    if (!empty($data['company_id'])) {
+      $notifabels = Member::find($data['company_id']);
+      $notificationData = [
+        'title' => " اضافة تعليق عن موظف ",
+        'body' => "قام " . auth('api')->user()?->first_name . ' ' . auth('api')->user()?->last_name . " باضافتكم كشركة عمل بها!",
+        'type' => 'experience',
+        'id_link' => $experience?->employee_id,
+      ];
+
+      \Illuminate\Support\Facades\Notification::send(
+        $notifabels,
+        new ClientNotification($notificationData, ['database', 'firebase'])
+      );
     }
 
-    public function get(Request $request, $member)
-    {
-        $paginateSize = $request->query('paginateSize', 10);
-        $member = Member::findOrFail($member);
-        return Experience::where('employee_id', $member->id)->with('company')->paginate($paginateSize);
-    }
+    return response()->json([
+      'message' => 'تم تحديث تفاصيل الخبرة بنجاح',
+      'experience' => $experience
+    ]);
+  }
 
-    public function getAll($member)
-    {
-        $member = Member::findOrFail($member);
-        return Experience::where('employee_id', $member->id)->with('company')->get();
-    }
-    public function getCitiesWorked($Member)
-    {
-        $Member = Member::find($Member);
-        return array_unique($Member->experience()->pluck('city')->toArray());
-    }
+  public function delete($experience)
+  {
+    $experience = Experience::findOrFail($experience);
+    $experience->delete();
 
-    public function show($experience)
-    {
-        $experience = Experience::findOrFail($experience);
-        return $experience;
-    }
-
-    public function update(Request $request,$experience)
-    {
-        // return $request ;
-        $data = $request->validate([
-            'title' => 'required|string|max:255',
-            'employment_type' => 'required|string|max:255',
-            'company_id' => 'required_if:custom_company_name,null|nullable|integer|exists:members,id,type,' . UserTypeEnum::COMPANY,
-            'custom_company_name' => 'required_if:company_id,null|string|max:255',
-             'city' => 'required|string|max:255',
-            'country' => 'required|string|max:255',
-            'location_type' => 'required|string|max:255',
-            'status_works' => 'required|string|max:255|in:work,notWork',
-            'still_working' => 'nullable|in:0,1',
-            'start_date' => 'required|numeric',
-            'end_date' => 'required_if:still_working,0|numeric',
-            'start_date_year' => 'required|numeric',
-            'end_date_year' => 'required_if:still_working,0|numeric|max:'.date("Y"),
-            'description' => 'required|string|max:255',
-            'profile_headline' => 'required|string|max:255',
-        ]);
-        $experience = Experience::findOrFail($experience);
-        $data['employee_id'] = auth('api')->user()->id;
-
-
-        
-        $experience->update($data);
-        # sending a notification to the user   
-        if(!empty($data['company_id'])) {
-        $notifabels = Member::find($data['company_id']);
-        $notificationData = [
-            'title' => " اضافة تعليق عن موظف ",
-            'body' => "قام " . auth('api')->user()?->first_name.' '.auth('api')->user()?->last_name . " باضافتكم كشركة عمل بها!",
-             'type' => 'experience',
-             'id_link' => $experience?->employee_id,     
-        ];
-
-        \Illuminate\Support\Facades\Notification::send(
-            $notifabels,
-            new ClientNotification($notificationData, ['database', 'firebase'])
-        );
-      }
-
-        return response()->json([
-            'message' => 'تم تحديث تفاصيل الخبرة بنجاح',
-            'experience' => $experience
-        ]);
-    }
-
-    public function delete($experience)
-    {
-        $experience = Experience::findOrFail($experience);
-        $experience->delete();
-
-        return response()->json([
-            'message' => 'تم حذف تفاصيل الخبرة بنجاح'
-        ]);
-    }
+    return response()->json([
+      'message' => 'تم حذف تفاصيل الخبرة بنجاح'
+    ]);
+  }
 }
