e52047210f7518f39b20966c7b9670d4024ce39f
Elsayed Rabeaa <131375012+ElsayedAbdo219@users.noreply.github.com>
2025-07-06 16:35:04 +0300

    5

--- database/migrations/2025_06_12_033835_delete_some_columns.php
+++ database/migrations/2025_06_12_033835_delete_some_columns.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-      #test yaml 4
+      #test yaml 5
         Schema::table('news', function (Blueprint $table) {
             $table->dropColumn('yes');
             $table->dropColumn('no');
