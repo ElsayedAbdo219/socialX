77966269f7f8c4689cab371cdc50ba939c282d6c
Elsayed Rabeaa <131375012+ElsayedAbdo219@users.noreply.github.com>
2025-07-18 00:16:19 +0300

    fix(follows):add follow is ? true or false

--- app/Models/Follow.php
+++ app/Models/Follow.php
@@ -7,17 +7,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Follow extends Model
 {
-    use HasFactory;
+  use HasFactory;
 
-    protected $guarded = [];
+  protected $guarded = [];
+  protected $appends = ['is_following_this_follower'];
 
-    public function userfollowed()
-    {
-        return $this->belongsTo(Member::class, 'followed_id');
+  public function getIsFollowingThisFollowerAttribute()
+  {
+    $authUser = auth('api')->user();
+
+    if (!$authUser) {
+      return false;
     }
+    return self::query()
+      ->where('follower_id', $authUser->id)
+      ->where('followed_id', $this->follower_id)
+      ->exists();
+  }
 
-    public function userfollower()
-    {
-        return $this->belongsTo(Member::class, 'follower_id');
-    }
+  public function userfollowed()
+  {
+    return $this->belongsTo(Member::class, 'followed_id');
+  }
+
+  public function userfollower()
+  {
+    return $this->belongsTo(Member::class, 'follower_id');
+  }
 }
