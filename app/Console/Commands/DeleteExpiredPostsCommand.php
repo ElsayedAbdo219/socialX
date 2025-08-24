8c808093fe84b86a641a0979594d15ac153bbb81
Elsayed Rabeaa <131375012+ElsayedAbdo219@users.noreply.github.com>
2025-08-24 10:52:03 +0300

    fix(deleteExpiredPosts):delete files and images

--- app/Console/Commands/DeleteExpiredPostsCommand.php
+++ app/Console/Commands/DeleteExpiredPostsCommand.php
@@ -4,33 +4,42 @@ namespace App\Console\Commands;
 
 use App\Models\Post;
 use App\Enum\PostTypeEnum;
+use Google\Service\Storage;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 
 class DeleteExpiredPostsCommand extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'app:delete-expired-posts-command';
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = 'app:delete-expired-posts-command';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = 'Command description';
 
-    /**
-     * Execute the console command.
-     */
-    public function handle()
-    {
-        $posts = Post::whereStatus(PostTypeEnum::ADVERTISE)
-            ->where('end_date', '<', now())
-            ->delete();
-        Log::info('Expired posts deleted successfully.');
+  /**
+   * Execute the console command.
+   */
+  public function handle()
+  {
+    $posts = Post::whereStatus(PostTypeEnum::ADVERTISE)
+      ->where('end_date', '<', now())
+      ->delete();
+      // dd($posts->count());
+      Log::info($posts);
+    foreach ($posts as $post) {
+      if ($post->image)
+        Storage::delete('posts/'.$post?->image);
+      if ($post->video)
+        Storage::delete('posts/'.$post?->file_name);
     }
+    Log::info('Expired posts deleted successfully.');
+  }
 }
