06eb06e3f63505ef23904aa445f3de017fdcaa22
Elsayed Rabeaa <131375012+ElsayedAbdo219@users.noreply.github.com>
2025-08-26 13:18:31 +0300

    fix(postsr):add soft deletes!

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,13 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory,
+    SoftDeletes;
+
     protected $guarded=[];
     protected $hidden = ['is_published'];
     protected $appends = ['total_shares'];
