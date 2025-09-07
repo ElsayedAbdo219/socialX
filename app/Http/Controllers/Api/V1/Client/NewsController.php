7e4ad03c447ff766d2195ce2dc0e625ad1ddb9ff
Elsayed Rabeaa <131375012+ElsayedAbdo219@users.noreply.github.com>
2025-09-07 23:27:29 +0300

    fix(news):add news filteration!

--- app/Http/Controllers/Api/V1/Client/NewsController.php
+++ app/Http/Controllers/Api/V1/Client/NewsController.php
@@ -9,27 +9,19 @@ use App\Http\Controllers\Controller;
 
 class NewsController extends Controller
 {
-  public function index(Request $request)
-  {
+public function index(Request $request)
+{
     $paginateSize = $request->query('paginateSize', 10);
     $location = GeoIP::getLocation(request()->ip())->country;
-    $newsLocation = [];
-    $newsWithoutLocation = [];
-    foreach (News::all() as $news) {
-      if ($news->countries == "all") {
-        $newsWithoutLocation [] = News::with('poll')
-          ->orderBy('id', 'desc')
-          ->where('countries', 'all')
-          ->paginate($paginateSize);
-      } else {
-        $newsLocation [] = News::with('poll')
-          ->whereJsonContains('countries', $location)
-          ->orderBy('id', 'desc')
-          ->paginate($paginateSize);
-      }
-    }
-    $newsTotal = array_merge($newsLocation, $newsWithoutLocation);
 
-    return response()->json($newsTotal);
-  }
+    $news = News::with('poll')
+        ->where(function ($query) use ($location) {
+            $query->where('countries', 'all')
+                  ->orWhereJsonContains('countries', $location);
+        })
+        ->orderBy('created_at', 'desc')
+        ->paginate($paginateSize);
+
+    return response()->json($news);
+}
 }
